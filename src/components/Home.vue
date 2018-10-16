cd8369a8cf11e91000731a35f7f65b17e84c8386
Alex Chan <czzsunset@gmail.com>
2018-10-16 20:48:58 +0800

    Add TabBar to home page

---
+++ src/components/Home.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+
+
+    <router-view></router-view>
+    <tabbar>    
+        <tabbar-item icon-class="info-circle" link="/wallet">
+           <x-icon type="ios-home" size="24" slot="icon"></x-icon>
+          <label slot="label">钱包</label>
+        </tabbar-item>
+        <tabbar-item link="/user">
+          <x-icon type="ios-person" size="28" class="icon-red" slot="icon"></x-icon>          
+          <label slot="label">个人</label>
+        </tabbar-item>
+    </tabbar>
+  </div>
+</template>
+
+<script>
+import { Group, Cell,  Tabbar, TabbarItem  } from 'vux'
+
+
+
+export default {
+  components: {
+    Group,
+    Cell,
+    Tabbar,
+    TabbarItem
+  },
+  data () {
+    return {
+      // note: changing this line won't causes changes
+      // with hot-reload because the reloaded component
+      // preserves its current state and we are modifying
+      // its initial state.
+      msg: 'Hello World!'
+    }
+  }
+}
+</script>
+
+<style>
+.vux-demo {
+  text-align: center;
+}
+.logo {
+  width: 100px;
+  height: 100px
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,31 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+
+import Home from '@/components/Home'
+import Wallet from '@/components/Wallet'
+// import User from '@/components/User'
+import User from '@/pages/user/User'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '',
+      name: 'Home',
+      component: Home,
+      children: [
+        {
+          path: 'wallet',
+          name: 'wallet',
+          component: Wallet
+        },
+        {
+          path: 'user',
+          name: 'user',
+          component: User
+        }
+      ]
     }
   ]
 })
