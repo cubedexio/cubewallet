f8416234c8e6e9f9cd4398d17a90600b7a2df4ea
leo <cheesekon@163.com>
2018-10-18 20:43:48 +0800

    启动页

--- src/components/account/Index.vue
+++ src/components/account/Index.vue
@@ -1,21 +1,24 @@
 <template>
   <div id="app">
-
-
+      <transition name="fade">
+        <div class="start" ref="start" v-if="show">
+          <img src="../../assets/images/start.jpg" alt="">
+        </div>
+      </transition>
       <group>
           Index Page
             <!-- <x-button type='primary' >login</x-button> -->
             <!-- <x-button type='default' link="/register">register</x-button> -->
 
-            
+
             <!-- <router-link  to="/register" class='to-hide'>Go to register</router-link> -->
             <!-- <router-view></router-view> -->
             <a href="/register">Go register</a>
-    
+
       </group>
 
 
-    
+
   </div>
 </template>
 <i18n>
@@ -28,11 +31,7 @@ Register:
 
 <script>
 import { Group, XButton, XInput, Cell, Tabbar, TabbarItem } from 'vux'
-
-
-
 export default {
-    
   components: {
     Group,
     XInput,
@@ -41,12 +40,24 @@ export default {
     Tabbar,
     TabbarItem
   },
-//   data () {
-
-//   }
+  data () {
+    return{
+      show:true
+    }
+  },
+  mounted(){
+    setTimeout(()=>{
+      this.show = false;
+    },2000)
+  }
 }
 </script>
 
 <style>
-
+  .fade-enter-active, .fade-leave-active {
+    transition: opacity .5s;
+  }
+  .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+    opacity: 0;
+  }
 </style>
