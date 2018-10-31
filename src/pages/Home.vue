0be9db8e7c10039dc6cf0eb562e126ecffc2a0e1
Alex Chan <czzsunset@gmail.com>
2018-10-31 14:36:19 +0800

    Fix bug of style scope

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -18,12 +18,9 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 .vux-demo {
   text-align: center;
 }
-.logo {
-  width: 100px;
-  height: 100px
-}
+
 </style>
--- src/pages/User.vue
+++ src/pages/User.vue
@@ -30,7 +30,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 .vux-demo {
   text-align: center;
 }
