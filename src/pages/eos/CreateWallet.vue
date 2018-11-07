b3fa4cab171f476e318c528788aad95da5043e42
Alex Chan <czzsunset@gmail.com>
2018-11-07 17:42:42 +0800

    增加EOS账户名字规范的提示

--- src/pages/eos/CreateWallet.vue
+++ src/pages/eos/CreateWallet.vue
@@ -12,8 +12,9 @@
           <!--</flexbox-item>-->
         <!--</flexbox>-->
       <flexbox orient="vertical" justify="space-around">
-        <flexbox-item>
+        <flexbox-item class="">
           <cube-input class="eos-account" title="" placeholder="请输入EOS帐号" v-model="eosname"></cube-input>
+          <label class="hint-label">EOS帐号名由小写字母和1-5组成的长度为12的字符串</label>
         </flexbox-item>
         <flexbox-item>
           <x-button type='default' @click.native="createEOSAccount">确认</x-button>
@@ -78,6 +79,17 @@ export default {
 
         },
         createEOSAccount() {
+            const eosPattern = /^[a-z1-5]{12}$/
+            if( this.eosname.match(eosPattern) == null ) {
+                this.$vux.toast.show({
+                    text: this.$t('Account name pattern incorrect' ),
+                    type: 'text',
+                    position: 'middle',
+                    width: '16rem'
+                })      
+                return           
+            }
+
             this.$vux.loading.show({
                 text: this.$t('Processing..')
             })
@@ -95,6 +107,8 @@ export default {
                     this.$vux.toast.show({
                         text:this.$t('Account name unavailabe, please input another one' ),
                         type:'text',
+                        position: 'middle',
+                        width: '16rem'                        
                     })
                 } else {
                     throw new Error("")
@@ -118,8 +132,11 @@ export default {
 <i18n>
 Account name unavailabe, please input another one:
   zh-CN: 用户名不可用，请使用其他用户名
+Account name pattern incorrect:
+    zh-CN: 用户名格式错误
 Processing..
   zh-CN: 处理中...
+
 
 </i18n>
 <style lang="less" scoped>
@@ -128,18 +145,21 @@ Processing..
     width: 100%;
     height: 100%;
     background: url("../../assets/images/sign_in_up.jpg") center 0  no-repeat ;
-    background-size: auto 100%;
+    background-size: cover;
 }
 
 .eos-account{
-    padding:1rem 3rem;
+    padding: 2rem 3rem 0 3rem;
 }
 
 
 
-label#hint-label {
+
+label.hint-label {
+    display: block;
+    padding:0rem 3rem 2rem 3rem;
     color: #fff;
-    font-size: 1.2em;
+    font-size: 0.7em;
 }
 
 
